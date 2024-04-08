703bc30103ea3b65a5bb9653ca431fc80b3a7686
davidlumbu <71940693+davidlumbu@users.noreply.github.com>
2024-04-07 22:22:45 -0400

    ajout d'un commentaire

--- flask_server.py
+++ flask_server.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Load your Joblib model
+# Load your Joblib model for url
 model = load('urlmodel.joblib')
 
 @app.route('/predict', methods=['POST'])
